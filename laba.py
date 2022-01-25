93c94bf163e049788928702f4a2e241b1bd4a6fa
denischagin <98116130+denischagin@users.noreply.github.com>
2022-01-25 23:54:25 +0300

    Update laba.py

--- laba.py
+++ laba.py
@@ -1,1 +1,16 @@
+x = int(input('Введите число:'))
+A = []
+def collatz(x):
+    global A
+    if x == 1:
+        A.append(int(x))
+        return x
+    elif x%2 == 0:
+        A.append(int(x))
+        return x2(x)
+    else:
+        A.append(int(x))
+        return x3_1(x)
 
+collatz(x)
+print(A)
